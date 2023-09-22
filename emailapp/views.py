ff5ce86a36c3231846a6d25eb52da2b62207940f
sabuhi.isazade@yahoo.com <sabuhi.isazade@yahoo.com>
2023-09-22 14:43:30 +0200

    adding subject_email

--- emailapp/views.py
+++ emailapp/views.py
@@ -5,10 +5,11 @@ from django.conf import settings
 def sendemail(request):
     if request.method == "POST":
         to = request.POST.get('toemail')
+        subject_email = request.POST.get('subject')
         content = request.POST.get('content')
         send_mail(
             #subject
-            "testing",
+            subject_email,
             #message
             content,
             #from email
