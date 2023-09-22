27c814acbbd2711b95a3bab8c9c0e9c6cfc2f9e8
sabuhi.isazade@yahoo.com <sabuhi.isazade@yahoo.com>
2023-09-22 12:28:28 +0200

    creating views/templates and url configration

--- emailapp/views.py
+++ emailapp/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def sendemail(request):
+    return render(request, 'email.html',{'title':'Send Email'})   
--- emailsender/urls.py
+++ emailsender/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from emailapp.views import sendemail
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('',sendemail,name="email")
 ]
