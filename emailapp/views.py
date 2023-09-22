15b0d4903edefa4723e62656c682bf368a7ee446
sabuhi.isazade@yahoo.com <sabuhi.isazade@yahoo.com>
2023-09-22 15:56:12 +0200

    adding add more email functionality

--- emailapp/views.py
+++ emailapp/views.py
@@ -2,33 +2,44 @@ from django.shortcuts import render
 from django.core.mail import send_mail
 from django.conf import settings
 
+
 def sendemail(request):
     if request.method == "POST":
-        to = request.POST.get('toemail')
         subject_email = request.POST.get('subject')
         content = request.POST.get('content')
-        send_mail(
-            #subject
-            subject_email,
-            #message
-            content,
-            #from email
-            settings.EMAIL_HOST_USER,
-            #recipent list
-            [to]
-        )
+
+        # Loop to handle dynamic email inputs
+        index = 1
+        while True:
+            email_key = f'toemail{index}'
+            to_email = request.POST.get(email_key)
+
+            # Break if there's no more email input with the current index
+            if not to_email:
+                break
+
+            send_mail(
+                subject_email,
+                content,
+                settings.EMAIL_HOST_USER,
+                [to_email]
+            )
+
+            # Increment the index to process the next email input
+            index += 1
+
         return render(
-        request,
-        'email.html',
-        {
-        'title':'send an email'
-        }
-    )
+            request,
+            'email.html',
+            {
+                'title': 'send an email'
+            }
+        )
     else:
         return render(
-        request,
-        'email.html',
-        {
-        'title':'send an email'
-        }
-    )
+            request,
+            'email.html',
+            {
+                'title': 'send an email'
+            }
+        )
